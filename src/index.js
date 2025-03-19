697cffd130bf82a49d32eb8d737d40d6700608e1
daisy <chpdaisy@gmail.com>
2025-03-19 08:12:26 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+document.addEventListener("DOMContentLoaded", () => { 
+  const form=document.getElementById('create-task-form');
+
+  form.addEventListener('submit', function(y){y.preventDefault()
+      addtask()
+  })
+  function addtask() {
+      const text=document.getElementById('new-task-description');
+      const tasktext=text.value.trim();
+      
+ const li = document.createElement('li');
+      li.textContent=tasktext;
+  
+  const deletebutton=document.createElement('button');
+         deletebutton.textContent='delete';
+      deletebutton.addEventListener('click', function(){
+          li.remove();
+      })
+      li.appendChild(deletebutton)
+      const tasklist=document.getElementById('tasks');
+      tasklist.appendChild(li);
+      
+      text.value='';
+  }
+})
+
